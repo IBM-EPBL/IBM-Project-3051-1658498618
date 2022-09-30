cdcbb98e8791fe4a20e7e72a9694f7225145711a
Hari-Prasad-GitHub <66509425+Hari-Prasad-GitHub@users.noreply.github.com>
2022-09-30 18:58:03 +0530

    Update IBM assignment2.py
    Bugs fixed

--- Assessments/ANNAMALAI S/Assignment 2/IBM assignment2.py
+++ Assessments/ANNAMALAI S/Assignment 2/IBM assignment2.py
@@ -1,13 +1,13 @@
 import random,time
 while(True):
-t=random.randint(0,35)
-time.sleep(1)
-if 10<t<30:
-print('normal')
-elif t>30:
-print('DANGER!!!')
-print('FIRE ALARAM ON...')
-else:
-print('unnatural temperature',t)
-print('FIRE ALARAM ON...')
-print('')
+  t=random.randint(0,35)
+  time.sleep(1)
+  if 10<t<30:
+    print('normal')
+  elif t>30:
+    print('DANGER!!!')
+    print('FIRE ALARAM ON...')
+  else:
+    print('unnatural temperature',t)
+    print('FIRE ALARAM ON...')
+  print('')
